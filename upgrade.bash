9e04997e79d474d346b621113e678e10e34a6a5d
Software Shinobi <the.software.shinobi@gmail.com>
2024-08-19 08:43:23 -0500

    automated terminal push

--- upgrade.bash
+++ upgrade.bash
@@ -22,7 +22,7 @@ npm install expo@latest --force
 npx expo install --fix -- --force
  
 
-npx expo-doctor@latest
+#npx expo-doctor@latest
 
 # https://docs.expo.dev/workflow/upgrading-expo-sdk-walkthrough/
 # https://expo.dev/changelog/2024/01-18-sdk-50
